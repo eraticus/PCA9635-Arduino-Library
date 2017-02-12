3b62dc602d93eab7a869fc786a3227e830dfa3fc
E M <eraticus@gmail.com>
2017-02-12 01:03:25 -1000

    updated example code.

--- examples/PCA9635/PCA9635.ino
+++ examples/PCA9635/PCA9635.ino
@@ -1,21 +1,30 @@
 #include <Wire.h>
 #include <PCA9635.h>
 
-const int ADDR = B1010101;
+const int ADDR = B0101010;
 
-PCA9635 leds = PCA9635(B1010101);
+PCA9635 leds = PCA9635(ADDR);
 
 void setup()
 {
   Wire.begin();
-  leds.begin();
+  Serial.begin(9600);
+  int ret = leds.begin();
+  Serial.println(ret);
+  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
+  digitalWrite(13, HIGH);
   leds.analogWrite(8, 0);
   leds.analogWrite(9, 0);
   leds.analogWrite(7, 0);
+  delay(250);
+  digitalWrite(13, LOW);
+  leds.analogWrite(8, 16);
+  leds.analogWrite(9, 16);
+  leds.analogWrite(7, 16);
   delay(250);
   leds.analogWrite(8, 64);
   leds.analogWrite(9, 64);
